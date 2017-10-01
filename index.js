8235f2584596a2f6f14f89141f2d4e77811d595e
Aslan1st <aslan1st@mac.com>
2017-10-01 05:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,11 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key){
+  return Object.assign({}, delete object.key)
+}
+
+function destructivelyDeleteFromObjectByKey(object, key){
+  
+}
